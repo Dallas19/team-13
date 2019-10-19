01ca26a5abac8c62403a70219e5bf019f4a4fe08
Chantha Mak <chantha.mak17@gmail.com>
2019-10-19 02:06:01 -0500

    work in progress 3.0 - finished the algo

--- firstMatchingAlgo.py
+++ firstMatchingAlgo.py
@@ -1,16 +1,5 @@
 import csv
 
-#class ranking():
-#     def __init__(self, ):
-"""with open("example_company.csv") as csvfile1:
-    readCSV1= csv.reader(csvfile1, delimiter=',')
-    counter = 0
-    titleEliminator = csv.reader(csvfile1)
-    jobCounter = []
-    for row in readCSV1:
-        comID = row[0]
-        jobCounter.append(comID)
-    numOfJob = len(jobCounter) """
 
 def firstMatchingAlgo():
     with open("example_student.csv") as csvfile2:
@@ -18,16 +7,14 @@ def firstMatchingAlgo():
         rankDict = {}
         finalArr = []
         next(readCSV2)
-        for row in readCSV2: #store -1 for no entry of the rank
+        for row in readCSV2:
             stuID = row[0]
             #jID = row[2]
             colCounter = 2
             stringConcat = ""
 
             while colCounter < len(row):
-                """if not(row[colCounter]):
-                    continue
-                else:"""
+
                 jID = row[colCounter]
                 stringConcat = str(jID) + '_'  + str(colCounter-1) + '_' + str(stuID)
                 finalArr.append(stringConcat)
@@ -35,10 +22,16 @@ def firstMatchingAlgo():
         for i in range(len(finalArr)):
             tempContainer = finalArr[i].split('_')
             rankDict[(tempContainer[0], tempContainer[1])] = tempContainer[2]
-        #sorted(rankDict.items(), key=lambda x: x[0])
-        #sorted(rankDict.items(), key=lambda s: s[0][1])
-        for k,v in sorted(rankDict.items(), key=lambda  x: x[0][0]):
+        rankDict.update(sorted(rankDict.items(), key=lambda x: x[0][0]))
+        rankDict.update(sorted(rankDict.items(), key=lambda s: s[0][1]))
+        # format of rankDict Dictionary
+         # (JOBID, Priority) -> StudentID
+        for k,v in sorted(rankDict.items()):
             print (k, v)
+    finalDict = {}
+    jobIDtracker = 0
+    for i in range(len(rankDict.keys()):
+        
 
 
 if __name__ == "__main__":
